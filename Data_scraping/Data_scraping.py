33e070c70d43abd5621d6e9b7c17b240360a2056
Michael Dodson <michaeljdodson42@gmail.com>
2024-11-27 15:22:27 -0500

    Using match statements instead of if/elif

--- Data_scraping/Data_scraping.py
+++ Data_scraping/Data_scraping.py
@@ -14,38 +14,48 @@ import scraping_functions as scrape
 
 # if the script is being executed as a "main" program
 if __name__ == "__main__":
+
     # determine which portion of code to run
     section_to_run = 6
-    if section_to_run == 1:
-        scrape.find_players("a", "z")
 
-    elif section_to_run == 2:
-        # returns large list containing smaller lists of 2 elements
-        player_list = scrape.find_players_by_year("a", "z", 1980, 1981)
+    match section_to_run:
+        case 1:
+            scrape.find_players("a", "z")
 
-    elif section_to_run == 3:
-        # returns large list containing smaller lists of 2 elements
-        player_list = scrape.find_players_by_year("a", "z", 1980, 1981)
+        case 2:
+            # returns large list containing smaller lists of 2 elements
+            player_list = scrape.find_players_by_year("a", "z", 1980, 1981)
 
-        # range (inclusive, exclusive)
-        year_range = range(1980, 1981)
-        scrape.get_player_season_stats(player_list, year_range)
+        case 3:
+            # returns large list containing smaller lists of 2 elements
+            player_list = scrape.find_players_by_year("a", "z", 1980, 1981)
 
-        # get the player-specific metrics
-        # player_metrics = scrape.get_player_metrics(player)
+            # range (inclusive, exclusive)
+            year_range = range(1980, 1981)
+            scrape.get_player_season_stats(player_list, year_range)
 
-    elif section_to_run == 4:
-        season_schedule = scrape.full_games_schedule(1980, 1981)
+            # get the player-specific metrics
+            # player_metrics = scrape.get_player_metrics(player)
 
-    elif section_to_run == 5:
-        # range (inclusive, exclusive)
-        set_range = range(1980, 1981)
-        game_data_df = scrape.collect_players_in_game(set_range)
+        case 4:
+            season_schedule = scrape.full_games_schedule(1980, 1981)
 
-        # pickle the data for later use
+        case 5:
+            # range (inclusive, exclusive)
+            set_range = range(1980, 1981)
+            game_data_df = scrape.collect_players_in_game(set_range)
 
-    elif section_to_run == 6:
-        scrape.pickled_players_in_games_to_csv()
+            # pickle the data for later use
 
-    else:
-        print("No section of code could run")
+        case 6:
+            scrape.pickled_players_in_games_to_csv()
+
+        case 7:
+            # print(scrape.get_team_abbreviations())
+            with open(
+                rf"C:\Users\Michael\Code\Python\Data_scraping\team_name_df.txt", "w"
+            ) as file:
+                file.write(scrape.get_team_abbreviations().to_string())
+
+        case _:
+            print("No section of code could run")
